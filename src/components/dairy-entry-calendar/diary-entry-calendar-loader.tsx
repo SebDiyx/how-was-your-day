7be3ff7bb5e7771673f9ffb331ecc79676159610
Diyx <cyclewithsebastian@gmail.com>
2025-05-29 20:16:24 +0800

    Add proper skeleton to dairy entry calendar

--- src/components/dairy-entry-calendar/diary-entry-calendar-loader.tsx
+++ src/components/dairy-entry-calendar/diary-entry-calendar-loader.tsx
@@ -1,10 +1,34 @@
 import { api } from '@/trpc/react';
 import { getVisibleRange } from './utils';
 import { DairyEntryCalendarDays } from './dairy-entry-calendar-days';
+import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
+import { eachDayOfInterval } from 'date-fns';
 
 type DairyEntryCalendarLoaderProps = {
     currentMonth: Date;
 };
+
+function DairyEntryCalendarSkeleton({ currentMonth }: { currentMonth: Date }) {
+    const visibleRange = getVisibleRange(currentMonth);
+    const visibleDays = eachDayOfInterval(visibleRange);
+
+    return (
+        <>
+            <div className="mt-2 grid grid-cols-7 gap-2">
+                {visibleDays.map((_, index) => (
+                    <Skeleton
+                        key={index}
+                        className={cn(
+                            'aspect-square rounded-xl',
+                            index === 0 && 'col-start-1',
+                        )}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
 
 export function DairyEntryCalendarLoader({
     currentMonth,
@@ -17,8 +41,7 @@ export function DairyEntryCalendarLoader({
     });
 
     if (isLoading) {
-        // TODO: Skeleton loader for the calendar days
-        return <div>Loading...</div>;
+        return <DairyEntryCalendarSkeleton currentMonth={currentMonth} />;
     }
 
     return (
